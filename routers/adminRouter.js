1cfc5a512febc1069fde74e45e95751977471588
Chris-Merced <christopher.r.merced@gmail.com>
2025-10-01 13:30:49 -0500

    Add route to make user an admin

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -5,6 +5,7 @@ const adminRouter = Router()
 adminRouter.delete("/message", adminController.deleteMessage)
 adminRouter.post("/ban", adminController.banUser)
 adminRouter.patch("/unban", adminController.unbanUser)
+adminRouter.patch("adminStatus", adminController.makeAdmin)
 
 module.exports = adminRouter
 
