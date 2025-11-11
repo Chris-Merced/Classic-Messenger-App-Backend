45a3c4aca8a67881147ce6758bef45a92457d88a
Chris-Merced <christopher.r.merced@gmail.com>
2025-11-11 12:44:46 -0600

    Use versioning to change messagesController to typescript

--- src/controllers/logoutController.ts
+++ src/controllers/logoutController.ts
@@ -1,31 +1,28 @@
 import * as db from "../db/queriesOld";
-import type {Response, Request} from "express"
-import {z} from "zod"
+import type { Response, Request } from "express";
+import { z } from "zod";
 import { checkErrorType } from "../authentication";
 
 const SessionCookieSchema = z.object({
-  sessionID: z.string()
-})
+  sessionID: z.string(),
+});
 
 export async function logoutUser(req: Request, res: Response) {
   try {
-
     const cookie = JSON.parse(req.cookies.sessionToken);
     const parsed = SessionCookieSchema.safeParse(cookie);
-    if(!parsed.success){
-      console.log("Error while logging out user: ")
-      console.log(z.treeifyError(parsed.error))
-      return res.status(400).json({error: z.treeifyError(parsed.error)})
+    if (!parsed.success) {
+      console.log("Error while logging out user: ");
+      console.log(z.treeifyError(parsed.error));
+      return res.status(400).json({ error: z.treeifyError(parsed.error) });
     }
-    const {sessionID} = parsed.data
+    const { sessionID } = parsed.data;
     await db.deleteSession(sessionID);
     res.status(200).json({ message: "Logout Successful" });
   } catch (err) {
-    const message  = checkErrorType(err)
+    const message = checkErrorType(err);
     console.error("Error in logging out user: " + message);
-    res
-      .status(500)
-      .json({ message: "Error in logging out user: " + message });
+    res.status(500).json({ message: "Error in logging out user: " + message });
   }
 }
 
