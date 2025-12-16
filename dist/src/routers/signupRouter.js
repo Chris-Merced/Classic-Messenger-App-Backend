e75f139afaa24e24e1c016256c061d545f05e57a
Chris-Merced <christopher.r.merced@gmail.com>
2025-12-16 12:55:01 -0600

    Modify signuprouter.ts name to signupRouter.ts

---
+++ dist/src/routers/signupRouter.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const signupRouter = (0, express_1.Router)();
+const signupController_1 = __importDefault(require("../controllers/signupController"));
+signupRouter.post('/', signupController_1.default.signupHandler);
+exports.default = signupRouter;
+//# sourceMappingURL=signupRouter.js.map
